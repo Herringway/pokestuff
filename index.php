bb98477cc07d889f005e91aafc3443f5b6524371
Cameron Ross <elpenguino@gmail.com>
2012-11-01 09:57:35 -0300

    Fix default game

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ set_error_handler('error_handler');
 set_exception_handler('exception_handler');
 $settings = new settings(array(
 		'Cache' => true,
-		'Default Game' => 'black 2',
+		'Default Game' => 'black',
 		'Default Mod' => 'stats',
 		'Default Language' => 'eng',
 		'Base Image URL' => '/static/images',
